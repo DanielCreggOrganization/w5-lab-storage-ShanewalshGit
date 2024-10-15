5c02286d2ba8f85a86e8be2af03d21bb89e8259b
Shane Walsh <83427120+ShanewalshGit@users.noreply.github.com>
2024-10-15 13:34:08 +0000

    Created App, implemented basic storage operations

---
+++ w-5-lab-storage/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'w-5-lab-storage',
+  webDir: 'www'
+};
+
+export default config;
